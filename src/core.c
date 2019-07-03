1209f22545162e94179f16cca844f63e5ecd0af9
lucas <lucaspaulom@hotmail.com>
2019-07-03 18:37:38 -0300

    first working version

--- src/core.c
+++ src/core.c
@@ -2,71 +2,65 @@
 #include <stdlib.h>
 #include <string.h>
 #include "includes/core.h"
-#include "includes/fila.h"
 
 void InitMorse (int imprimir) {
     int i;
-    char letra, *entrada, morse[10], linhaMorse[1000], linhaAlfanumerica[1000];
-    Fila *f; FFVazia(f);
-    Arvore *arvore = CriaArvore();
+    char letra, *entrada, morse[10], linhaMorse[200], linhaAlfanumerica[200];
+    Arvore arvore = CriaArvore();
     FILE *morseTxt = fopen("morse.txt", "r");;
-
-    while(fscanf(morseTxt, " %c %s", &letra, morse) != EOF) {  
-        Chave *chave;
-        strcpy(chave->chave, morse);
-        chave->letra = letra;
+    Chave chave;
+    while(fscanf(morseTxt, " %c %s", &letra, morse) != EOF) {
+        strcpy(chave.chave, morse);
+        chave.letra = letra;
         Insere(&arvore, chave);
     }
     fclose(morseTxt);
 
-    while (fgets(linhaMorse, 500, stdin) != NULL) {
-        char *codigoMorse;
+    char *codigoMorse;
+    while (fgets(linhaMorse, 200, stdin) != NULL) {
         i = 0;
         if(linhaMorse[strlen(linhaMorse)-1] == '\n') {
             linhaMorse[strlen(linhaMorse)-1] = '\0';
         }
-        codigoMorse = strtok(linhaMorse, " ");    //getting the first morse codigoMorse
+        codigoMorse = strtok(linhaMorse, " ");
         while (codigoMorse != NULL) {
-            //if it's an '/' just put a blank space on message
-            if(strcmp(codigoMorse, "/") == 0) {
+            if(!strcmp(codigoMorse, "/")) {
                 linhaAlfanumerica[i] = ' ';
             }
             else {
-                linhaAlfanumerica[i] = PesquisaR(arvore, codigoMorse, 0);
+                linhaAlfanumerica[i] = Pesquisa(arvore, codigoMorse)->registro.letra;
             }
             codigoMorse = strtok(NULL, " ");
             i++;
         }
         linhaAlfanumerica[i] = '\0';
-        InsereFila(f, linhaAlfanumerica);
+        printf("%s\n", linhaAlfanumerica);
+        // armazenar as linhaAlfanumerica em um vetor...
     }
-
-    ImprimeFila(f);
 
     if(imprimir)
         ImprimeArvore(arvore);
 
-    LiberaFile(f);
     LiberaArvore(arvore);
 }
 
-Arvore *CriaNo (Chave *reg) {
-    Arvore *nova = (Arvore*) malloc(sizeof(Arvore));
+Apontador CriaNo (Chave reg) {
+    Apontador nova = (Apontador) malloc(sizeof(No));
     nova->registro = reg;
     nova->esq = NULL;
     nova->dir = NULL;
     return nova;
 }
 
-Arvore *CriaArvore () { 
-    Arvore *nova = (Arvore*) malloc(sizeof(Arvore));
-    nova->registro->letra = '!';
+Apontador CriaArvore () { 
+    Apontador nova = (Apontador) malloc(sizeof(No));
+    nova->registro.letra = '!';
     nova->esq = NULL;
     nova->dir = NULL;
     return nova;
 }
 
-void LiberaArvore (Arvore *a) {
+void LiberaArvore (Apontador a) {
     if(a != NULL) {
         LiberaArvore(a->esq);
         LiberaArvore(a->dir);
@@ -74,23 +68,21 @@ void LiberaArvore (Arvore *a) {
     }
 }
 
-void ImprimeArvore (Arvore *a) {
+void ImprimeArvore (Apontador a) {
     if(a != NULL) {
-        if(a->registro->letra != '!') {
-            printf("%c %s\n", a->registro->letra, a->registro->chave);
+        if(a->registro.letra != '!') {
+            printf("%c %s\n", a->registro.letra, a->registro.chave);
         }
         ImprimeArvore(a->esq);
         ImprimeArvore(a->dir);
     }
 }
 
-char PesquisaR (Arvore *a, char *chave, int p) {
-    if (a->esq == NULL && a->dir == NULL) {
-        if (!strcmp(a->registro->chave, chave)) {
-            char letra = a->registro->letra;
-            return letra;
-        }
-        else { return '!'; }
+Apontador PesquisaR (Apontador a, char *chave, int p) {
+    if (a == NULL) { return NULL; }
+    if (strlen(chave) == p) {
+        if (!strcmp(a->registro.chave, chave)) { return a; }
+        else { return NULL; }
     }
 
     if (chave[p] == '.') {
@@ -100,9 +92,13 @@ char PesquisaR (Arvore *a, char *chave, int p) {
     }
 }
 
-Arvore *InsereR (Arvore *a, Chave *reg, int p) {
+Apontador Pesquisa (Apontador a, char *chave) {
+    return PesquisaR(a, chave, 0);
+}
+
+Arvore InsereR (Arvore a, Chave reg, int p) {
     char chave[10];
-    strcpy(chave, reg->chave);
+    strcpy(chave, reg.chave);
 
     if (strlen(chave) == p) {
         if (a == NULL) { return CriaNo(reg); }
@@ -122,6 +118,6 @@ Arvore *InsereR (Arvore *a, Chave *reg, int p) {
 }
 
 
-void Insere (Arvore **a, Chave *reg) {
+void Insere (Arvore *a, Chave reg) {
     *a = InsereR(*a, reg, 0);
 }
--- src/includes/core.h
+++ src/includes/core.h
@@ -1,31 +1,33 @@
-struct registro {
+typedef struct {
     char chave[10];
     char letra;
-};
+} Chave;
 
-typedef struct registro Chave;
+typedef struct no *Apontador;
 
-struct no {
-    struct no *esq;
-    struct no *dir;
-    Chave *registro;
-};
+typedef struct no {
+    Apontador esq;
+    Apontador dir;
+    Chave registro;
+} No;
 
-typedef struct no Arvore;
+typedef Apontador Arvore;
 
 // #1:
 void InitMorse (int imprimir);
 // #2:
-Arvore *CriaNo (Chave *reg);
+Apontador CriaNo (Chave reg);
 // #3:
-Arvore *CriaArvore ();
+Apontador CriaArvore ();
 // #4:
-void LiberaArvore (Arvore *a);
+void LiberaArvore (Apontador a);
 // #5:
-void ImprimeArvore (Arvore *a);
+void ImprimeArvore (Apontador a);
 // #6:
-char PesquisaR (Arvore *a, char *chave, int p);
+Apontador PesquisaR (Apontador a, char *chave, int p);
 // #7:
-Arvore *InsereR (Arvore *a, Chave *reg, int p);
+Apontador InsereR (Apontador a, Chave reg, int p);
 // #8:
-void Insere (Arvore **a, Chave *reg);
+void Insere (Apontador *a, Chave reg);
+// #9:
+Apontador Pesquisa (Apontador a, char *chave);
