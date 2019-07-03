3fd6720562958df2f9ebf99671573aedca5354ae
lucas <lucaspaulom@hotmail.com>
2019-07-02 21:55:02 -0300

    Core functions

--- src/core.c
+++ src/core.c
@@ -8,6 +8,41 @@ void initMorse () {
     printf("Init program \n");
 }
 
+Arvore *criaNo(Chave *reg) {
+    Arvore *nova = (Arvore*) malloc(sizeof(Arvore));
+    nova->registro = reg;
+    //setting left and right Arvores to NULL to prevent errors
+    nova->esq = NULL;
+    nova->dir = NULL;
+    return nova;
+}
+
+Arvore *CriaArvore() { 
+    Arvore *nova = (Arvore*) malloc(sizeof(Arvore));
+    nova->registro->letra = '!';
+    nova->esq = NULL;
+    nova->dir = NULL;
+    return nova;
+}
+
+void LiberaArvore(Arvore *a) {
+    if(a != NULL) {
+        LiberaArvore(a->esq);
+        LiberaArvore(a->dir);
+        free(a);
+    }
+}
+
+void imprimeArvore(Arvore *a) {
+    if(a != NULL) {
+        if(a->registro->letra != '!') {
+            printf("%c %s\n", a->registro->letra, a->registro->chave);
+        }
+        imprimeArvore(a->esq);
+        imprimeArvore(a->dir);
+    }
+}
+
 char pesquisaR(Arvore *a, char *chave, int p) {
     if (p == NULL) { return NULL; }
     
@@ -26,39 +61,28 @@ char pesquisaR(Arvore *a, char *chave, int p) {
     }
 }
 
-// A ..-.
-// B -...
-// C -.-.
-// D -
-// E .
-// F .-
-// G --
-// H ....
-// I ..-
-// J .---
-// K -.-
-// L .-..
-// M -.
-// N --.
-// O ---
-// P .--.
-// Q --.-
-// R .-.
-// S ...
-// T -..
-// U ..
-// V ...-
-// W .--
-// X -..-
-// Y -.--
-// Z --..
-// 0 -----
-// 1 .----
-// 2 ..---
-// 3 ...--
-// 4 ....-
-// 5 .....
-// 6 -....
-// 7 --...
-// 8 ---..
-// 9 ----.
+Arvore *insereR (Arvore *a, Chave *reg, int p) {
+    char chave[10];
+    strcopy(chave, reg->chave);
+
+    if (strlen(chave) == p) {
+        if (a == NULL) { return criaNo(reg); }
+        else {
+            a->registro = reg;
+            return a;
+        }
+    }
+    if (a == NULL) { a = criaArvore(); }
+    if (!strcmp(chave[p], '.')) {
+        a->esq = insereR(a->esq, reg, p+1);
+    }
+    else {
+        a->dir = insereR(a->dir, reg, p+1);
+    }
+    return a;
+}
+
+
+void insere (Arvore **a, Chave *reg) {
+    *a = insereR(*a, reg, 0);
+}
